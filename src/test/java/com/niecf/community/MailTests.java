87d245a2df7bb197e09bcd93b3ea34214dfdab79
niecf <602065880@qq.com>
2023-03-08 20:51:43 +0800

    补充email

---
+++ src/test/java/com/niecf/community/MailTests.java
@@ -0,0 +1,33 @@
+package com.niecf.community;
+
+import com.niecf.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("602065880@qq.com","TEST","Welcome.");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context=new Context();
+        context.setVariable("username","niecf");
+        String content=templateEngine.process("/mail/demo",context);
+        System.out.println(content);
+        mailClient.sendMail("602065880@qq.com","HTML",content);
+    }
+}
