4e32e9ec9ba5af5bd29a603a5d45dd26095e62a2
niecf <602065880@qq.com>
2023-03-21 18:24:29 +0800

    补充login

---
+++ src/main/java/com/niecf/community/util/CommunityConstant.java
@@ -0,0 +1,11 @@
+package com.niecf.community.util;
+
+public interface CommunityConstant {
+    int ACTIVATION_SUCCESS=0;
+    int ACTIVATION_REPEAT=1;
+    int ACTIVATION_FAILURE=2;
+
+    int DEFAULT_EXPIRED_SECONDS=3600*12;
+
+    int REMENBER_EXPIRED_SECONDS=3600*24*100;
+}
